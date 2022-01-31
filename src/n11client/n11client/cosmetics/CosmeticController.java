16658a29b06e073e0cdcc128eead6b5ae591ffcd
FiRe <levi@null-llc.com>
2022-01-30 19:31:16 -0600

    A select few have the hat now

--- src/n11client/n11client/cosmetics/CosmeticController.java
+++ src/n11client/n11client/cosmetics/CosmeticController.java
@@ -4,7 +4,7 @@ import net.minecraft.client.entity.AbstractClientPlayer;
 
 public class CosmeticController {
 
-    public static boolean shouldRenderTopHat(AbstractClientPlayer player) { return true; }
+    public static boolean shouldRenderTopHat(AbstractClientPlayer player) { return (player.getName() == "FiReLScar" || player.getName() == "4tl0renz0" || player.getName() == "myst303"); }
 
     public static float[] getTopHatColor(AbstractClientPlayer player) { return new float[] {0, 0, 0}; }
 
